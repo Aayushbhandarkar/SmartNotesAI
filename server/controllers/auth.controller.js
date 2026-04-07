90e7cfe44c7e80610b5927b9b8e3622f702c4a91
Ayush Bhandarkar <ayushbhandarkar7@gmail.com>
2026-04-07 06:13:32 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -12,15 +12,14 @@ export const googleAuth = async (req,res) => {
                 name , email
             })
         }
-        let token = await getToken(user._id)
-        res.cookie("token" , token , {
-            httpOnly:true,
-            secure:true,
-            sameSite:"none",
-            path:"/",
-            maxAge:7 * 24 * 60 * 60 * 1000
-
-        })
+    let token = await getToken(user._id)
+res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    path: "/",  // ✅ Change this from "./" to "/"
+    maxAge: 7 * 24 * 60 * 60 * 1000
+})
         return res.status(200).json(user)
     } catch (error) {
         return res.status(500).json({message:`googleSignup Error  ${error}`})
